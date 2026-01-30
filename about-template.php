85a73cf5bb56c3fd3967b373c82c010034bde129
oliinykroman <oliinykroman@gmail.com>
2026-01-30 21:31:17 +0200

    add wrap for blog-post slider

--- about-template.php
+++ about-template.php
@@ -24,6 +24,17 @@ get_template_part( 'template-parts/modules/section', 'hero', ['id' => $p_id] );
 
 <section class="team-section">
     <div class="container">
+        <ul class="breadcrumbs">
+            <li class="crumb-item">
+                <a href="#">About us</a>
+            </li>
+
+            <li class="crumb-item">
+                <span>
+                    <?php the_title()?>
+                </span>
+            </li>
+        </ul>
         <div class="content-section">
             <div class="container">
                 <div class="content-wrap">
--- template-parts/modules/__blog-item-preview.php
+++ template-parts/modules/__blog-item-preview.php
@@ -1,6 +1,7 @@
 <a href="<?php echo $permalink?>" class="blog-post-preview">
-    <img src="<?php echo esc_url( $thumbnail )?>" alt="<?php echo esc_attr( $title )?>">
-
+    <div class="card-item">
+        <img src="<?php echo esc_url( $thumbnail )?>" alt="<?php echo esc_attr( $title )?>">
+    </div>
     <div class="item-description">
         <h4 class="item-title">
             <?php echo $title?>
